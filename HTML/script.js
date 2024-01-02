caa7727f13848c34a238621094b6014722ced3a6
Castoridae78 <Dirk.Bieber@gmail.com>
2024-01-02 21:48:00 +0100

    zwischenstand

--- HTML/script.js
+++ HTML/script.js
@@ -1,4 +1,13 @@
-document.getElementById('box').addEventListener('click', function() {
-    alert('Herzlich willkommen auf meiner Website!');
-  });
-  
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait for the DOM content to be loaded
+
+    // Get the button element
+    var myButton = document.getElementById("myButton");
+
+    // Add a click event listener to the button
+    myButton.addEventListener("click", function() {
+        // Call your custom function or add your script logic here
+        alert("Button clicked! Add your script logic here.");
+    });
+});
